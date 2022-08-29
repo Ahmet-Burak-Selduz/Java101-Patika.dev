7acbb70b3946667b47b1383be41b518c990b9388
Ahmet-Burak-Selduz <111522636+Ahmet-Burak-Selduz@users.noreply.github.com>
2022-08-29 17:01:30 +0300

    init

--- Sayi_Siralama/src/Main.java
+++ Sayi_Siralama/src/Main.java
@@ -10,23 +10,23 @@ public class Main {
         b = input.nextInt();
         System.out.print("Sayı Girin : ");
         c = input.nextInt();
-        if (a>b & a>c) {
-            if (b>c) {
-                System.out.print("a > b > c");
+        if (a<b & a<c) {
+            if (b<c) {
+                System.out.print("a < b < c");
             } else {
-                System.out.print("a > c > b");
+                System.out.print("a < c < b");
             }
-        } else if (b>a & b>c) {
-            if (a>c) {
-                System.out.print("b > a > c");
+        } else if (b<a & b<c) {
+            if (a<c) {
+                System.out.print("b < a < c");
             } else {
-                System.out.print("b > c > a");
+                System.out.print("b < c < a");
             }
         } else {
-            if (a>b) {
-                System.out.print("c > a > b");
+            if (a<b) {
+                System.out.print("c < a < b");
             } else {
-                System.out.print("c > b > a");
+                System.out.print("c < b < a");
             }
         }
     }
